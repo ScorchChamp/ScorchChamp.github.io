934ba0564d01f0a7fab0f4db3530a6765ceb892e
Lars Cornelissen <l.cornelissen5@student.han.nl>
2023-01-06 21:54:09 +0100

    Added tags to repos

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const author = "ScorchChamp";
 async function getPages() {
     const reposResponse = await fetch(`https://api.github.com/users/${author}/repos`);
     const repos = await reposResponse.json();
-    return repos.filter(repo => repo.has_pages && repo.topics.includes("tool")).map(repo => parseRepo(repo));
+    return repos.filter(repo => repo.has_pages).map(repo => parseRepo(repo));
+}
+
+function parseTopics(topics) {
+    return topics.map(topic => `<div class="topic">${topic}</div>`).join('');
 }
 
 function parseRepo(repo) {
-    const url = `https://${author}.github.io/${repo.name}`;
+    console.log(repo)
     return `
         <div class="repo-container">
-            <a href="${url}" target="_blank">
+            <a href="https://${author}.github.io/${repo.name}" target="_blank">
                 <div class="repo">
-                    <h2>${repo.name}</h2>
+                    <h2 class="repo-name">${repo.name}</h2>
+                    <div class="topics">${parseTopics(repo.topics)}</div>
                     <p>${repo.description}</p>
+                    <h5 class="last-updated">Last update <span class="last-updated-time">${parseTime(repo.updated_at)}</span></h5>
                 </div>
             </a>
             <a href="${repo.html_url}" target="_blank">
@@ -30,6 +36,20 @@ async function displayPages() {
     const pages = await getPages();
     const container = document.querySelector('.container');
     container.innerHTML = pages.join('');
+}
+
+function parseTime(timeString) {
+    const time = new Date(timeString);
+    const currentTime = new Date();
+    const diff = currentTime - time;
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(diff / (1000 * 60));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    return seconds < 60 ? `${seconds} seconds ago` :
+        minutes < 60 ? `${minutes} minutes ago` :
+            hours < 24 ? `${hours} hours ago` :
+                time.toDateString();
+
 }
 
 displayPages();
